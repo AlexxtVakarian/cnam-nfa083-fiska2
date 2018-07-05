d7a98aeb8ab2605cc299915629544ca0aee16a3a
AlexxtVakarian <alexissfiska@gmail.com>
2018-07-05 15:49:12 +0200

    utilisation de pdo et effectivement mot de pass root de mon sql pourtant je ne mets jamais de mdp sur mes test en local... réussite problème d'affichage des articles détectés.
    Signed-off-by: AlexxtVakarian <alexissfiska@gmail.com>

--- inc/connexion.inc.php
+++ inc/connexion.inc.php
@@ -1,12 +1,17 @@
-<?php
 
-	$host = "locahost";
-	$user = "user";
-	$pass = "root";
-	$bdd = "cnamcp09_nfa083cf1_2017"
 
-	$conn = mysql_connect($host,$user,$pass,) or die(mysql_error());
 	
-	mysql_select_db($bdd,$conn); 
+	<?php
+	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;      
+	$pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND] = 'SET NAMES utf8'; 
+	
+	$serveur = 'localhost';
+	$bddname = 'cnamcp09_nfa083cf1_2017';
+	$loginserveur = 'user';
+	$mdpserveur = 'root';
+	
+	$con = new PDO('mysql:host='.$serveur.';dbname='.$bddname,$loginserveur, $mdpserveur, $pdo_options);
+	
+
 
 ?>
